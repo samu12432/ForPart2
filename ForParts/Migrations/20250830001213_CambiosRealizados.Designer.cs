dda450c763582d18ebdb60e46dde2c21f5f799e2
RobertoAlvarenga18 <132228005+RobertoAlvarenga18@users.noreply.github.com>
2025-08-29 21:21:36 -0300

    Nuevabase

--- ForParts/Migrations/20250830001213_CambiosRealizados.Designer.cs
+++ ForParts/Migrations/20250830001213_CambiosRealizados.Designer.cs
@@ -4,6 +4,7 @@ using ForParts.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ForParts.Migrations
 {
     [DbContext(typeof(ContextDb))]
-    partial class ContextDbModelSnapshot : ModelSnapshot
+    [Migration("20250830001213_CambiosRealizados")]
+    partial class CambiosRealizados
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ForParts/Migrations/20250830001213_CambiosRealizados.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForParts.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambiosRealizados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "glassThickness",
+                table: "Glass",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.CreateTable(
+                name: "Budgets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdCustomer = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budgets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Budgets_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Formulas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CodigoInsumo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoInsumo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SeriePerfil = table.Column<int>(type: "int", nullable: false),
+                    TipoProducto = table.Column<int>(type: "int", nullable: false),
+                    Expresion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Formulas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BudgetedProduct",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PresupuestoId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Width = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Heigth = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    ProductType = table.Column<int>(type: "int", nullable: false),
+                    SerieProfile = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    BudgetId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetedProduct", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BudgetedProduct_Budgets_BudgetId",
+                        column: x => x.BudgetId,
+                        principalTable: "Budgets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BudgetedSupply",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BudgetedProductId = table.Column<int>(type: "int", nullable: false),
+                    SupplyCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TypeSupply = table.Column<int>(type: "int", nullable: false),
+                    UnitMeasure = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    amount = table.Column<int>(type: "int", nullable: false),
+                    UnityPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Subtotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OutOfStock = table.Column<bool>(type: "bit", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetedSupply", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BudgetedSupply_BudgetedProduct_BudgetedProductId",
+                        column: x => x.BudgetedProductId,
+                        principalTable: "BudgetedProduct",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetedProduct_BudgetId",
+                table: "BudgetedProduct",
+                column: "BudgetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BudgetedSupply_BudgetedProductId",
+                table: "BudgetedSupply",
+                column: "BudgetedProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budgets_CustomerId",
+                table: "Budgets",
+                column: "CustomerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BudgetedSupply");
+
+            migrationBuilder.DropTable(
+                name: "Formulas");
+
+            migrationBuilder.DropTable(
+                name: "BudgetedProduct");
+
+            migrationBuilder.DropTable(
+                name: "Budgets");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "glassThickness",
+                table: "Glass",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
