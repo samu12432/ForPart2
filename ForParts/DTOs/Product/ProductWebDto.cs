859b11d8f6936a02a98c83f2a884c52cf54d1d0a
samu12432 <samuelarregin2@gmail.com>
2025-09-08 23:59:08 -0300

    Create ProductWebDto.cs
    EndPoint para web

---
+++ ForParts/DTOs/Product/ProductWebDto.cs
@@ -0,0 +1,26 @@
+﻿using ForParts.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForParts.DTOs.Product
+{
+    public class ProductWebDto
+    {
+        [Required(ErrorMessage = "Es necesario ingresar el nombre del producto.")]
+        [MaxLength(100)]
+        public string productName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Es necesario ingresar una descripcion del producto.")]
+        [MaxLength(100)]
+        public string productDescription { get; set; } = string.Empty;
+
+        [EnumDataType(typeof(ProductType), ErrorMessage = "Tipo de producto no valido.")]
+        public ProductType productCategory { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que cero.")]
+        public decimal productPrice { get; set; }
+
+        public IFormFile? Image { get; set; }
+
+        public string? imageUrl { get; set; } = string.Empty;
+    }
+}
