e0f41b9a4ae65909cf319fe20936fe33dd328207
samu12432 <samuelarregin2@gmail.com>
2025-09-08 23:59:21 -0300

    Update ProductMapper.cs
    EndPoint para web

--- ForParts/Mappers/Product/ProductMapper.cs
+++ ForParts/Mappers/Product/ProductMapper.cs
@@ -1,6 +1,7 @@
 ﻿using ForParts.DTO.Product;
 using AutoMapper;
 using ForParts.Models.Product;
+using ForParts.DTOs.Product;
 
 namespace ForParts.Mappers.Product
 {
@@ -10,6 +11,14 @@ namespace ForParts.Mappers.Product
         {
             CreateMap<Models.Product.Product, ProductDto>().ReverseMap();
             CreateMap<SupplyNecessary, SupplyNecessaryDto>().ReverseMap();
+
+            CreateMap<Models.Product.Product, ProductWebDto>()
+                .ForMember(dest => dest.productName, opt => opt.MapFrom(src => src.productName))
+                .ForMember(dest => dest.productDescription, opt => opt.MapFrom(src => src.productDescription))
+                .ForMember(dest => dest.productCategory, opt => opt.MapFrom(src => src.productCategory))
+                .ForMember(dest => dest.productPrice, opt => opt.MapFrom(src => src.productPrice))
+                .ForMember(dest => dest.imageUrl, opt => opt.MapFrom(src => src.imageUrl))
+                .ForAllMembers(opt => opt.Ignore());
         }
     }
 }
