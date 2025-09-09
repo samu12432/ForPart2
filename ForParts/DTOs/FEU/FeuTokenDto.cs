3edcf0cff1be27a499fa711bfd6538c9552564f2
samu12432 <samuelarregin2@gmail.com>
2025-09-09 00:26:08 -0300

    Create FeuTokenDto.cs
    Emitir Facturas a traves de FEU

---
+++ ForParts/DTOs/FEU/FeuTokenDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ForParts.DTOs.FEU
+{
+    public class FeuTokenDto
+    {
+        public string access_token { get; set; } = string.Empty;
+        public string token_type { get; set; } = string.Empty;
+        public string refresh_token { get; set; } = string.Empty;
+    }
+}
